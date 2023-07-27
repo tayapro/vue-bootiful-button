a905787b6d38817b76fa72d3e0ab0914b73a35c7
Taya Simakova <stipaxa@gmail.com>
2023-07-27 18:19:53 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import Button from './components/ButtonWithIconAndText.vue'
+import { AcademicCapIcon as Icon } from '@heroicons/vue/24/solid'
+</script>
+
+<template>
+    <!-- <div class="center-by-position"> -->
+    <div class="center-by-flex">
+        <Button prop_text="Hello, world!" :prop_icon="Icon" />
+    </div>
+</template>
+
+<style scoped>
+.box {
+    border: 1px solid black;
+    width: 300px;
+}
+
+.center-by-flex {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+.center-by-position {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+</style>
---
+++ src/components/ButtonWithIconAndText.vue
@@ -0,0 +1,40 @@
+<script setup>
+defineProps(['prop_text', 'prop_icon'])
+</script>
+
+<template>
+    <button>
+        <div class="button-with-icon">
+            <component :is="prop_icon" class="icon" />
+            <p>{{ prop_text }}</p>
+        </div>
+    </button>
+</template>
+
+<style scoped>
+.button-with-icon {
+    display: flex;
+    padding: 0.3rem;
+    align-items: center;
+    cursor: pointer;
+    gap: 0.5rem;
+    color: #f5f5f5;
+}
+
+button {
+    border: none;
+    background-color: #009999;
+    border-radius: 5px;
+}
+
+button:hover {
+    background-color: #0099cc;
+    border-color: #0099cc;
+}
+
+.icon {
+    width: 1.5rem;
+    height: 1.5rem;
+    color: #f5f5f5;
+}
+</style>
